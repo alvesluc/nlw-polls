3acbad2ac27765b77b5b94a1595ec50d106bdb25
Lucas Alves <42820357+alvesluc@users.noreply.github.com>
2024-02-06 18:25:45 +0000

    added  and  configurations

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,27 @@
 import fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
+import { z } from "zod";
 
 const app = fastify();
+
+const prisma = new PrismaClient();
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running!");
 });
 
-app.get("/hello", () => {
-  return "Hello NLW";
+app.post("/polls", async (request, response) => {
+  const createPollBody = z.object({
+    title: z.string(),
+  });
+
+  const { title } = createPollBody.parse(request.body);
+
+  const poll = await prisma.poll.create({
+    data: {
+      title,
+    },
+  });
+
+  return response.status(201).send({ poll_id: poll.id });
 });
